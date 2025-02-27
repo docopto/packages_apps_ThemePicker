3c6fc87c0a4231d1e2135ed8ac7dd2267250fc82
f104a <f104a@f104a.io>
2025-02-27 12:42:56 +0100

    Add QS gradient [4/4]
    This commit adds a possibility to make QuckSettings buttons and brightness slider background filled with gradient(instead of just solid) with help of an overlay. The user-facing switch is added to theming module and a package itself is added Lineage overlays. Also SystemUI is modified to factor out shape of brightness slider in order to minimize hardcoding in overlay.
    The values of dimens in overlay are hardcoded as SystemUI does not publish it. While they do not overlayed anywhere in device/OS tree it may become problem in future.
    
    The resulting QS screenshot: https://imgur.com/a/RFdraYg
    
    The changes done to following repos:
    * frameworks/base
    * vendor/lineage
    * packages/overlays/Lineage
    * packages/apps/ThemePicker
    
    Change-Id: I5bfe6e0623155d5baa56dbc672b4772860e28b4d

---
+++ src/com/android/customization/picker/qs/ui/view/QSSectionView.kt
@@ -0,0 +1,13 @@
+package com.android.customization.picker.qs.ui.view
+import android.content.Context
+import android.util.AttributeSet
+import com.android.wallpaper.picker.SectionView
+
+class QSSectionView(
+    context: Context?,
+    attrs: AttributeSet?,
+) :
+    SectionView(
+        context,
+        attrs,
+    )
